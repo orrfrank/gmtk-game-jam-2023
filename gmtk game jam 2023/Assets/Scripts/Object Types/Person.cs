01eb1814ad1d66945ca8553ad44cf65b1f1b3291
orrfrank <101966625+orrfrank@users.noreply.github.com>
2023-07-08 15:32:24 +0300

    does this work
    prob not

--- gmtk game jam 2023/Assets/Scripts/Object Types/Person.cs
+++ gmtk game jam 2023/Assets/Scripts/Object Types/Person.cs
@@ -18,7 +18,7 @@ public class Person : MonoBehaviour
     Rigidbody2D rb;
     bool isWaitingForElevator = false;
     bool isInElevator;
-    [SerializeField] Vector2 targetPosition;
+    [SerializeField] protected Vector2 targetPosition;
     const int BORDER_MEXICO = 1;
     const int BORDER_CANADA = 13;
     int direction = 1;
@@ -106,6 +106,10 @@ public class Person : MonoBehaviour
     //====================================
     //the code bellow is responsible for the grouping up
     //the idea is to have a group leader and for everyone in the group to follow him
+    public void Charge(GameObject target)
+    {
+        (target.GetComponent("Person") as MonoBehaviour).enabled = false;
+    }
 
     void CheckForGroupers()
     {
